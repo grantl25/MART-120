d846ae4e2e1209ede22cffd806578dc500d2af98
grantl25 <gl140927@umconnect.umt.edu>
2024-11-12 22:25:34 -0700

    Add files via upload

---
+++ Homework 9/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  circle(200,200,200);
+  circle(200,200,20)
+  triangle(50, 175, 40, 200, 186, 45)
+   square(60,300,280);
+    ellipse(200,250,70,30);
+  ellipse(160,150,30,20);
+  ellipse(240,150,30,20);
+    point(30, 75);
+   line(300, 500, 300, 300);
+  line(100,500,100,300)
+  line(125,140,80,100)
+  text('Grant Larson', 10, 30);
+}
